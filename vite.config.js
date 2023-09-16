34cc8667fc31a336b3e7dbe2891aff71465a393e
AVMaslennikov <AVMaslennikov@inno.tech>
2023-09-16 16:57:47 +0300

    fix path to publicUrl

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
 import {defineConfig} from "vite";
 
 export default defineConfig({
-	base: "/",
+	base: "/scuba-diving/",
 	build: {
 		sourcemap: true,
 		// Reduce bloat from legacy polyfills.
 		target: "esnext",
 		// Leave minification up to applications.
 		minify: false,
+		assetsDir: './'
 	},
 	plugins: []
 })
